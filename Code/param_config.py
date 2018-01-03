2689c095c5ffc05512403f8d0a2e5a5d97c5729f
moneyml <maoli2017@163.com>
2018-01-03 08:20:45 +0800

    2018 first

--- Code/param_config.py
+++ Code/param_config.py
@@ -24,9 +24,9 @@ class ParamConfig:
         ## path
         self.data_folder ='../../Data'
         self.feat_fold = feat_folder
-        self.original_train_data_path = "%s/train_drop_constant.csv"% self.data_folder
-        self.original_test_data_path = "%s/test_a_drop_constant.csv"% self.data_folder
-        self.original_test_data2_path = "%s/test_b_drop_constant.csv"% self.data_folder
+        self.original_train_data_path = "%s/train_non_missing.csv"% self.data_folder
+        self.original_test_data_path = "%s/test_a_non_missing.csv"% self.data_folder
+        self.original_test_data2_path = "%s/test_b_non_missing.csv"% self.data_folder
 
 
 
